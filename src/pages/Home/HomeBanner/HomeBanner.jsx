4ef6b2c3f0c47466150703d76838c5edb6c8a546
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-25 01:03:34 +0600

    try to add slick-slider

--- src/pages/Home/HomeBanner/HomeBanner.jsx
+++ src/pages/Home/HomeBanner/HomeBanner.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+
+
 
 const HomeBanner = () => {
+
+  // slider settings
+
+
+
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vitae. Autem repudiandae tempore ullam dolorem commodi sed fugiat quibusdam reiciendis alias eum obcaecati quis facilis consequuntur laboriosam recusandae placeat eaque fugit, velit voluptatibus voluptas numquam eius saepe voluptatum! Beatae doloremque dicta voluptatibus mollitia pariatur natus veniam, assumenda repellendus reprehenderit sapiente facilis itaque voluptatum asperiores magni, perspiciatis illum at rerum odit? Mollitia ipsum nisi, ratione dolores, sapiente ex, praesentium adipisci saepe fugit repudiandae ipsa similique inventore. Explicabo itaque ipsum, accusantium quam aut, blanditiis deleniti perferendis eveniet culpa ab dicta voluptatem, maxime animi enim distinctio nobis architecto deserunt! Quas reiciendis maxime quae illo consequuntur ab fuga neque, error, doloremque corrupti quis optio. Facere sapiente quidem quia dolor quibusdam porro libero hic quod nemo veritatis, placeat maiores aliquid in atque. Totam nobis inventore consequuntur assumenda consequatur pariatur voluptate doloribus voluptatibus ullam eligendi voluptates, dolorum at beatae tempore nihil illum eius veritatis, molestiae, quis vitae quod dolores commodi officia! Quibusdam odio rem optio est veritatis accusantium quasi consectetur quisquam velit eaque ipsum neque, inventore harum nostrum amet id magnam maiores! Veritatis saepe facilis fugiat provident, quod doloremque cupiditate nulla corrupti laborum autem enim ut aliquid similique. Labore, eveniet. Assumenda at ratione quod placeat! Eveniet.</p>
+    <div className='overflow-hidden'>
+      {/* content section start */}
+      
+      {/* content section end */}
     </div>
   );
 };
