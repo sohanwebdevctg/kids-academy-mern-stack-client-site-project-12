9e544077da57f61698790bf999179a925f23c99e
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-24 21:33:51 +0600

    create errorpage white mobile responsive

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import ErrorPage from "../components/ErrorPage";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    errorElement: <p>Error...</p>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
+        element: <h1>this is home page</h1>,
+      },
+      {
+        path: "/loading",
         element: <h1>this is home page</h1>,
       },
     ],
