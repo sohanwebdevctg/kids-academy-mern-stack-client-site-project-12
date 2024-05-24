9490c688cbe4fed2eb9f5b605a21c8336d23875c
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-24 22:53:51 +0600

    adding footer white mobile responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ import logo from '../../../public/logo/logo.png'
 
 const Footer = () => {
   return (
-    <div className="">
+    <div className="bg-black p-3 sm:p-10 md:p-8 lg:p-10 xl:p-12">
       {/* footer content start */}
       <div className="container mx-auto">
         {/* footer section start */}
         {/* top footer start */}
-        <div className="p-3 md:p-8 grid grid-cols-2 gap-5 md:flex justify-between items-center">
+        <div className="p-3 md:p-8 grid grid-cols-2 gap-5 md:flex justify-between items-center ">
           {/* item1 start */}
           <div>
             <ul className="space-y-1">
@@ -17,7 +17,7 @@ const Footer = () => {
               src={logo}
               className="w-16 h-10 sm:w-24 sm:h-12 md:w-24 md:h-10 lg:w-24 lg:h-12 xl:w-24 xl:h-12"
             ></img></li>
-            <li><p className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base">
+            <li><p className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200">
               C-10, Avenue, Issakhan<br></br>Chittagong, Bangladesh
             </p></li>
             </ul>
@@ -34,25 +34,25 @@ const Footer = () => {
             <ul className="space-y-1">
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Online Contact
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               24/7 Support
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Online Class
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
             conceptual Session
             </Link></li>
@@ -71,25 +71,25 @@ const Footer = () => {
             <ul className="space-y-1">
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Home
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Instructor
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Classes
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Services
             </Link></li>
@@ -108,25 +108,25 @@ const Footer = () => {
             <ul className="space-y-1">
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Terms of use
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Privacy policy
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               FAQ
             </Link></li>
               <li><Link
               to="/"
-              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base"
+              className="text-sm sm:text-base md:text-sm lg:text-base xl:text-base text-slate-200"
             >
               Social Policy
             </Link></li>
@@ -136,7 +136,7 @@ const Footer = () => {
           {/* item4 end */}
         </div>
         {/* top footer end */}
-        <div className="divider px-6 md:px-0"></div>
+        <hr className=" border-x-[1px] border-gray-400 w-full my-6 "></hr>
         {/* bottom footer start */}
         {/* bottom footer end */}
         <footer className="footer flex justify-center sm:justify-between items-center flex-col sm:flex-row px-5">
@@ -150,7 +150,7 @@ const Footer = () => {
             >
               <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
             </svg>
-            <p className="xl:text-base">Copyright © 2024 - All right reserved</p>
+            <p className="xl:text-base text-slate-200">Copyright © 2024 - All right reserved</p>
           </aside>
           <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
             <a>
