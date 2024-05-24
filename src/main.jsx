6c4c21d6360dbf33fa6c403eef5e08770703f148
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-25 01:22:47 +0600

    remover slick slider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+
 import { RouterProvider} from "react-router-dom";
 import { router } from './router/Router.jsx';
 
--- src/pages/Home/HomeBanner/HomeBanner.jsx
+++ src/pages/Home/HomeBanner/HomeBanner.jsx
@@ -1,16 +1,15 @@
 
 
 
+
 const HomeBanner = () => {
 
   // slider settings
 
-
-
   return (
     <div className='overflow-hidden'>
       {/* content section start */}
-      
+
       {/* content section end */}
     </div>
   );
